9d5f40fc52ced8632ff4787294d727e38c971224
Cyrus <cy.vattes@gmail.com>
2017-03-02 00:09:14 +0000

    Fix transposition errors (6720 & 6725), condense labels, remove unused lines and comments

--- src/main.py
+++ src/main.py
@@ -11,7 +11,8 @@ import cpe_modules as cpe
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 size = 8.8  # This is kept at full-scale for visibility
-src = cv2.flip(cv2.imread("../static/input/pattern.jpg"), 0)    #Flip fixes rotation problem
+srcPath = "../static/input/pattern.jpg"
+outPath = "../static/output/display.svg"
 imgPaths = ["../static/img/IMG_6719.JPG",
             "../static/img/IMG_6720.JPG",
             "../static/img/IMG_6721.JPG",
@@ -21,31 +22,45 @@ imgPaths = ["../static/img/IMG_6719.JPG",
             "../static/img/IMG_6727.JPG"
            ]
 
-
 # Set Axis Labels and Limits, Plot SRC Image
+print "START"
+print "Loading source image: %s" % (srcPath)
+src = cv2.flip(cv2.imread(srcPath), 0)    #Flip fixes rotation problem
 ax.set_xlabel('X-axis')
 ax.set_ylabel('Y-axis')
-ax.set_zlabel('Z-axis')
+ax.set_zticklabels([])
+ax.set_zticks([])
+# ax.set_zlabel('Z-axis')   # Z-axis labels gets in the way of scatter labels
 ax.set_xlim(-50, 50)
 ax.set_ylim(-50, 50)
-ax.set_zlim(0, 50)     # Z-axis lower limit == 0
+ax.set_zlim(0, 100)     # Z-axis lower limit == 0
 X, Y = np.meshgrid(np.linspace(-size, size, src.shape[0]), np.linspace(-size, size, src.shape[0]))
 Z = 0
+print "Plotting source image (QR Code)"
 ax.plot_surface(X, Y, Z, facecolors=src/255.0, shade=False)
 
 
 for imgPath in imgPaths:
     # Read SRC Pattern and Img (test1)
-    # img = cv2.imread(imgPath)
+    print ">>>>>>>>>>>>>>>>>>>>>>>>>"
+    print "Reading Image File: %s" % (imgPath)
     img = cv2.flip(cv2.transpose(cv2.imread(imgPath)), 1)   #Flip fixes rotation problem
     
     # Find Marks from QR Code
+    print "Detecting QR Code Markers"
     bounds = cpe.findMarks(img)
+    print "Detecting Camera Pose and Orientation"
     P, V = cpe.reconstruct(bounds, (img.shape))
-    
-    # Plot Camera Pose (P) and Orientation Vector (V)
-    cpe.plot(P, V, imgPath, ax)
+    print "Pose = " + np.array_str(P)
+    print "Orientation = " + np.array_str(V)
 
+    # Plot Camera Pose (P) and Orientation Vector (V)
+    print "Plotting Pose and Orientation"
+    cpe.plot(P, V, imgPath, ax)
+    print "<<<<<<<<<<<<<<<<<<<<<<<<<\n"
+    
 # Display Output
 # plt.show()        # Disabled for non-Xwindow use
-plt.savefig('../static/output/display.svg')
+plt.savefig("../static/output/display.svg")
+print "Output saved as %s" % (outPath)
+print "DONE"
